aa47af799c4f418af2cebb8105c265e462f5ed43
dev-aleks <a7ex.ap@gmail.com>
2021-02-16 08:50:53 +0300

    drawing rect with hatching

--- lab3/pygame_1.py
+++ lab3/pygame_1.py
@@ -5,7 +5,7 @@ import pygame
 pygame.init()
 
 # Set screen:
-screen_width = 700
+screen_width = 400
 screen_height = 400
 screen = pygame.display.set_mode([screen_width, screen_height])
 
@@ -18,7 +18,19 @@ BLUE = (0, 0, 255)
 
 # drawing...
 screen.fill(BLACK)
-pygame.draw.circle(screen, RED, (350, 200), 100)
+x1 = 100
+y1 = 100
+dx = 200
+dy = 200
+# для rect параметра, от левого верхнего угла: [влево, вниз, длина по X, длина по Y]:
+pygame.draw.rect(screen, WHITE, [x1, y1, dx, dy], 1)
+# рисуем штриховку:
+h = 10
+N = dx // h
+x = x1 + h
+for i in range(N):
+    pygame.draw.line(screen, WHITE, [x, y1], [x, y1 + dy], 1)
+    x += h
 
 # Обновить экран для отображения
 pygame.display.update()
