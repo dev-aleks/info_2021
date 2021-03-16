09a5bcc33547688c16553d68713017e870ef2116
dev_aleks <a7ex.ap@gmail.com>
2021-03-16 07:45:38 +0300

    draw circle with randomint, future = add mouse event

--- events/draw_2.py
+++ events/draw_2.py
@@ -1,11 +1,12 @@
 import pygame
+from random import randint
 
 # initial the game:
 pygame.init()
 
 # set screen:
-screen_width = 200
-screen_height = 200
+screen_width = 1200
+screen_height = 900
 screen = pygame.display.set_mode([screen_width, screen_height])
 
 # variable color:
@@ -14,11 +15,27 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+
+def new_ball():
+    '''
+    Функция для рисования нового шарика. 
+    Размеры рандомные и автоматические.
+    Координаты рисования автоматические
+    '''
+    x = randint(100, 1100)
+    y = randint(100, 900)
+    r = randint(10, 100)
+    color = COLORS[randint(0,5)]
+    pygame.draw.circle(screen, color, (x, y), r)
 
 # loop until the user clicks the close button:
 done = False
 clock = pygame.time.Clock()
-max_FPS = 30
+max_FPS = 2
 
 while not done:
     # this limits the while loop to a max of 10 times per second.
@@ -28,22 +45,15 @@ while not done:
     for event in pygame.event.get():    # user did something
         if event.type == pygame.QUIT:   # if user clicked close
             done = True                 # change flag; exit from loop
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:       #   events for left button
-                pygame.draw.circle(screen, RED, event.pos, 50)
-                pygame.display.update()
-            elif event.button == 2:
-                pygame.draw.circle(screen, BLUE, event.pos, 30)
-                pygame.display.update()
-            elif event.button == 3:     #   events for right button
-                pygame.draw.circle(screen, GREEN, event.pos, 50)
-                pygame.display.update()
+
     # drawing...
     # set dcreen background:
-    screen.fill(WHITE)
+    screen.fill(BLACK)
+    # drawing ball:
+    new_ball()
 
     # after all drawing commans update screen:
-    # pygame.display.update()
+    pygame.display.update()
 
 # be IDLE friendly
 pygame.quit()
