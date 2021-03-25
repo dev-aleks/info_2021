2073e1cacbdcf82a8fa4351ef71d23bc59533611
dev_aleks <a7ex.ap@gmail.com>
2021-03-25 10:00:34 +0300

    реализован счетчик для очков на экране

--- events/draw_2.py
+++ events/draw_2.py
@@ -3,6 +3,7 @@ from random import randint
 
 # initial the game:
 pygame.init()
+pygame.font.init()
 
 # set screen:
 screen_width = 1000
@@ -20,40 +21,58 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
+# common variable
+count = 0  # for calculate count (score)
+count_ball = 0  # for calculate - how many i get new_ball()
+
+
 def new_ball():
-    '''
+    """
     Функция для рисования нового шарика. 
     Размеры рандомные и автоматические.
     Координаты рисования автоматические
-    '''
+    """
     global x, y, r
     x = randint(100, screen_width)
     y = randint(100, screen_height)
     r = randint(10, 100)
-    color = COLORS[randint(0,5)]
+    color = COLORS[randint(0, 5)]
     pygame.draw.circle(screen, color, (x, y), r)
 
+
 def click(event):
-    '''
+    """
     Функция для получения координат круга
-    '''
+    """
     return x, y, r
 
-def calculate_and_compare(x1, y1, R1, x2, y2):
-    '''
-    Функция для подсчета и сравнения расстояния между двумя точками
+
+def calculate_and_compare(x1, y1, x2, y2):
+    """
+    Функция для подсчета расстояния между двумя точками
     :param x1: координата x круга
     :param y1: координата y круга
     :param R1: значение радиуса круга
     :param x2: координата x нажатия
     :param y2: координата y нажатия
     :return: результат, попал ли в круг
-    '''
-    R2 = (((x2-x1)**2) + ((y2-y1)**2))**(0.5) # length between point
-    if R2 <= R1:
-        print('good')
-    else:
-        print ('no good')
+    """
+    length_between_point = (((x2 - x1) ** 2) + ((y2 - y1) ** 2)) ** (0.5)  # length between point
+    return length_between_point
+
+
+def draw_text(text, size, x, y):
+    """
+    Функция для отрисовки динамичного текста
+
+    :param text: текст для отображения
+    :param size: размер шрифта
+    :param x, y: привязка от левого верхнего угла для размещения текста
+    """
+    my_font = pygame.font.Font(None, size)
+    my_text = my_font.render(text, True, WHITE, None)
+    screen.blit(my_text, (x, y))  # blit для отображения текста в координатах
+
 
 # loop until the user clicks the close button:
 done = False
@@ -65,27 +84,32 @@ while not done:
     # Leave this out and we will use all CPU we can.
     clock.tick(max_FPS)
     # exit cycle:
-    for event in pygame.event.get():    # user did something
-        if event.type == pygame.QUIT:   # if user clicked close
-            done = True                 # change flag; exit from loop
+    for event in pygame.event.get():  # user did something
+        if event.type == pygame.QUIT:  # if user clicked close
+            done = True  # change flag; exit from loop
+            print(count, ' / ', count_ball)  # show result
 
-        #   что должно происходить при нажатии кнопки?
-        #   1) необходимо вычислить координаты круга и его радиус, зафиксировав эти значения
-        #   2) необходимо вычислить позицию нажатия мыши
-        #   3) вычислить расстояние между двумя точками - результат добавляет очко, если оно меньше или равно радиусу круга
+        # if event is mouse "button down":
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            click(event)
-            event.pos
-            calculate_and_compare(x, y, r, event.pos[0], event.pos[1])
+            click(event)  # show option BALL from new_ball()
+            event.pos  # calculate position my mouse
+            length = calculate_and_compare(x, y, event.pos[0], event.pos[1])  # calculate length between two point
+            # give count:
+            if length <= r:
+                count += 1
 
     # drawing...
     # set screen background:
     screen.fill(BLACK)
     # drawing ball:
     new_ball()
+    count_ball += 1
+    # visual score:
+    draw_text(str(count), 30, 10, 10)
 
     # after all drawing commans update screen:
     pygame.display.update()
 
 # be IDLE friendly
+pygame.font.quit()
 pygame.quit()
